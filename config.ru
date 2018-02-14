2a851dd59228187522ad5d8cdd7f5e6efbd7a90e
Mark Eschbach <mark@rhumbix.com>
2018-02-14 10:01:54 -0800

    Adds abaility to query a preconfigured Vault host

--- config.ru
+++ config.ru
@@ -1,4 +1,3 @@
 require_relative 'emeta'
 
-puts `ifconfig`
 run EMeta
--- emeta.rb
+++ emeta.rb
@@ -29,4 +29,42 @@ class EMeta < Sinatra::Base
 		body = HTTParty.get( url, timeout: 1 ).body
 		url + "\n" + body
 	end
+
+	get "/vault" do
+		# Nonce
+		nonce = File.read( ENV['NONCE_FILE'] ).strip! if ENV['NONCE_FILE']
+		nonce = ENV['NONCE'] if ENV['NONCE']
+
+		# Hosts
+		hosts = [ ENV['VAULT_HOST'] ] if ENV['VAULT_HOST']
+		if ENV['VAULT_HOSTS_FILE']
+			vault_hosts_file = File.read( ENV['VAULT_HOSTS_FILE'] )
+			hosts = vault_hosts_file.split( "\n" ).map { |line| line.split( " " )[0] }
+		end
+
+		# Certificate
+		pkcs7 = HTTParty.get( 'http://169.254.169.254/latest/dynamic/instance-identity/pkcs7' ).body.split( "\n" ).join("")
+
+		url = "https://" + hosts[0] + ":8200" + "/v1/auth/aws-ec2/login"
+		body = HTTParty.post( url,
+			:body => {
+				:role => "engine-role",
+				:nonce => nonce,
+				:pkcs7 => pkcs7
+			}.to_json,
+			:headers => { 'Content-Type' => 'application/json' },
+			:verify => false
+		).body
+
+		content_type 'application/json'
+		{
+			:url => url,
+			:inputs => {
+				:hosts => hosts,
+				:nonce => nonce,
+				:pkcs7 => pkcs7
+			},
+			:body => body
+		}.to_json
+	end
 end
